042813ec87cff4ffefd0082b0167a3e7dd43d3de
Tristan <tfarneau@gmail.com>
2020-02-18 14:21:07 +0100

    fix

--- src/engine/Controls.js
+++ src/engine/Controls.js
@@ -44,7 +44,7 @@ export default class Controls {
     document.addEventListener("touchend", (e) => {
       console.log(e)
       if(e.changedTouches[0].clientY < (viewport.h*0.8)){
-        this.enableTurbo()
+        this.disableTurbo()
       }
     })
     
